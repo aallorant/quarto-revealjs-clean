6dd276d227d1e29d04d2525160e53174aaeef32e
aallorant <aallorant@reed.edu>
2025-04-24 09:19:02 +0100

    slides for UoM presentation

---
+++ presentation-UoM-research-agenda.R
@@ -0,0 +1,51 @@
+
+# Outline {background-color="#40666e"}
+
+::: incremental
+
+- **Motivating data**
+  
+  - **Survey sampling**
+  * Simple Random Sampling
+* Stratified Sampling
+* Cluster Sampling
+
+- **Modes of inference**
+  * Model-based
+* Design-based
+
+:::
+  
+  ## Motivating Question: Measuring healthcare quality
+  
+  ::: incremental
+
+- 5 million of people die from poor‑quality care every year worldwide
+* Poor‑quality care is widespread and a top concern for governments
+
+- Characterising and understanding variations in access to quality healthcare quality is important to reduce deaths amenable to healthcare
+
+- **High‑quality healthcare** is defined as *“Health services that increase the likelihood of desired health outcomes and are consistent with current professional knowledge.”* (Institute of Medicine)  
+
+- **How do we measure quality of care?**
+  * Structures $\iff$ material infrastructures that condition an environment conducive or inimical to the provision of good care   
+* Processes $\iff$ a direct measure of health providers’ practices
+* Outcomes  $\iff$ changes in health status attributable to biological and socio-economic factors, and antecedent care
+
+:::
+  
+  ## Health Facility Surveys
+  
+  ::: incremental
+
+- In many low to middle-income countries (LMICs), routine health information systems are lacking or incomplete
+
+- Most accurate information about healthcare accessibility and quality in LMICs comes from health facility surveys
+* USAID's Service Provision Assessment surveys (SPA)
+  * World Bank's Service Delivery Indicators (SDI)
+* World Health Organisation's Service Access and Readiness Assessment (SARA)
+
+- SPA: stratified surveys collecting information on service availability and quality of care; 35 surveys conducted in 18 countries since 1997. 
+  * quality of care indicators include structural quality (physical and human resources) and process quality (provision of care).
+
+:::
